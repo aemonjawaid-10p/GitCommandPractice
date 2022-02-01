ad358c1b739ab554ca7521dbb6f0228e76dde943
aemonjawaid-10p <79843444+aemonjawaid-10p@users.noreply.github.com>
2022-02-01 13:37:00 +0500

    initial commit

--- app/src/main/java/com/example/gitcommandpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitcommandpractice/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         System.out.println("New test branch")
+        System.out.println("New test branch")
     }
 }
