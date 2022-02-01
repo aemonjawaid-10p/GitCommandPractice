fcb4c6089d23c48c543210e00d4f4b34718fc13d
aemonjawaid-10p <79843444+aemonjawaid-10p@users.noreply.github.com>
2022-02-01 13:40:50 +0500

    add commit to test branch

--- app/src/main/java/com/example/gitcommandpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitcommandpractice/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        System.out.println("New test branch")
-        System.out.println("New test branch")
+        System.out.println("Test Branch")
     }
 }
